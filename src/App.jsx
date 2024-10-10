dce353b896a37f42194b3610a5b94a82a4a102a2
David Torres <david.martinezezguerra@gmail.com>
2024-10-10 18:16:14 -0500

    init proyect

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+//import { useState } from 'react'
+import { TableRepository } from './TableRepository'
+
+function App() {
+  return (
+    <>
+     <section>
+      <div>
+        <h2></h2>
+        <p></p>
+      </div>
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <button></button>
+      </div>
+     </section>
+     <section>
+      <div>
+        <input type="text" />
+        <button></button>
+      </div>
+      <div>
+        <TableRepository></TableRepository>
+      </div>
+     </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/TableRepository.jsx
@@ -0,0 +1,6 @@
+function TableRepository() {
+    return(
+        <div></div>
+    )
+}
+export {TableRepository}

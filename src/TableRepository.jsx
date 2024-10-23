2ee1c0728f7565bc520f764d6cb0fecb42cfaa54
David Torres <david.martinezezguerra@gmail.com>
2024-10-23 17:17:27 -0500

    tabla de repositorios

--- src/TableRepository.jsx
+++ src/TableRepository.jsx
@@ -1,6 +1,15 @@
-function TableRepository() {
+function TableRepository({nameRepository, descriptionRepository, languageRepository}) {
+    //console.log(props.dataRepos)
     return(
-        <div></div>
+        <>
+                <div className="container-repos-data">
+                    <span>{nameRepository}</span>
+                    <span>{descriptionRepository}</span>
+                    <span>{languageRepository}</span>
+                </div>
+            
+            
+        </>
     )
 }
 export {TableRepository}

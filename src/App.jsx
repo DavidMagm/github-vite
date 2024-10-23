df62830bd7c87c65dc4eb6ff22d3b29f86d76910
David Torres <david.martinezezguerra@gmail.com>
2024-10-23 17:15:22 -0500

    creando estado de carga y error

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,65 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import { TableRepository } from './TableRepository'
+import './App.css'
 
 function App() {
+
+  const [textSearch, setTextSearch] = useState("");
+  const [dataReposUser, setDataReposUser] = useState([]);
+  const [filterRepository, setFilterRepository] = useState("");
+  const [error, setError] = useState(false);
+
+  async function infoUser() {
+    const rolName = document.querySelector(".rol-name")
+
+    if(textSearch == "") {
+      return alert("Pon un valor")
+    }
+
+    const res = await fetch(`https://api.github.com/users/${textSearch}/repos`)
+    if(res.status === 200) {
+      const data = await res.json()
+      setError(false)
+      setDataReposUser(data) 
+      rolName.textContent = textSearch 
+    } else {
+      setError(true)
+      setDataReposUser([])
+      rolName.textContent = 'El nombre no existe'
+    }
+    
+  };
+  
+  let filterNameRepository = dataReposUser.filter(data => {
+    let nameRepository = data.name.toLowerCase() 
+    let searchNameRepository = filterRepository.toLowerCase()
+    return nameRepository.includes(searchNameRepository)
+  } 
+  )  
+
   return (
     <>
-     <section>
-      <div>
-        <h2></h2>
-        <p></p>
+     <section className='container-section user-name'>
+      <div className='container-rol-name'>
+        <h2 className='rol-name'></h2>
       </div>
-      <div>
-        <input type="text" />
-        <input type="text" />
-        <button></button>
+      <div className='form-search-user'>
+        <input className='value-name' type="text" onChange={(e) => setTextSearch(e.target.value)}/>
+        <button onClick={infoUser}>Search</button>
       </div>
      </section>
-     <section>
-      <div>
-        <input type="text" />
-        <button></button>
+     <section className='container-section filter-repository'>
+      <div className='search-filter'>
+        <input className='value-search-repository' type="text" onChange={(e) => setFilterRepository(e.target.value)}/>
       </div>
-      <div>
-        <TableRepository></TableRepository>
-      </div>
+     </section>
+     <section className='container-table-repository'>
+      {error && <p>El nombre no existe</p>}
+        <div className='table-repository'>
+          {filterNameRepository.map((data, index) => (
+          <TableRepository key={index} nameRepository={data.name} descriptionRepository={data.description} languageRepository={data.language}></TableRepository>
+          ))}
+        </div>
      </section>
     </>
   )
